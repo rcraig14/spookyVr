821f5fdeb3e5759a29dd4e4b27fd5bfb5837afb7
Oasis14 <hkyryan14@gmail.com>
2017-11-16 12:11:45 -0700

    Created a door open script. Going to add more functions

---
+++ Assets/Scripts/doorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		openDoor ();
+	}
+
+	void openDoor(){
+		var target = Quaternion.Euler (0, 90, 0);
+		transform.localRotation = Quaternion.Slerp (transform.localRotation, target, Time.deltaTime * 2);
+	}
+
+	void closeDoor(){
+
+	}
+}
