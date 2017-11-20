740f3482d18d8047f2b5eff826ee0b5a5f597f82
Oasis14 <hkyryan14@gmail.com>
2017-11-19 18:14:59 -0800

    Updated function to open and close door. To use change the public variable to the desired state

--- Assets/Scripts/doorScript.cs
+++ Assets/Scripts/doorScript.cs
@@ -3,23 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class doorScript : MonoBehaviour {
-
 	// Use this for initialization
+	public string doorState;
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		openDoor ();
+		changeDoorState (doorState);
+
 	}
 
-	void openDoor(){
-		var target = Quaternion.Euler (0, 90, 0);
-		transform.localRotation = Quaternion.Slerp (transform.localRotation, target, Time.deltaTime * 2);
+	/* changeDoor(string state)
+	 * Called every update.
+	 * In order to change this the public variable doorState needs to be changed
+	 */
+	void changeDoorState(string state){
+		float rotation = 0;
+		int speed = 0;
+		string audioClip = "";
+		switch (state) {
+		case "open":
+			rotation = 90;
+			speed = 5;
+			audioClip = "open";
+			break;
+		case "close":
+			rotation = 0;
+			speed = 5;
+			audioClip = "open";
+			break;
+		case "slam":
+			rotation = 0;
+			speed = 15;
+			audioClip = "slam";
+			break;
+
+		case "slowOpen":
+			rotation = 90;
+			speed = 1;
+			audioClip = "slowOpen";
+			break;
+		}
+		doorRotation (rotation, speed);
+	}
+
+	void doorRotation(float yRotation, int speed){
+		var target = Quaternion.Euler (0, yRotation, 0);
+		if (Quaternion.Angle (transform.localRotation, target) > 1) {
+			transform.localRotation = Quaternion.RotateTowards (transform.localRotation, target, speed);
+		} 
 	}
 
 	void closeDoor(){
-
+		//var target = Quaternion.Euler (0, 0, 0);
+		//transform.localRotation = Quaternion.Slerp (transform.localRotation, TargetJoint2D, Time.deltaTime * 2);
 	}
+
+
 }
