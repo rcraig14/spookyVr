159ca69eab57cd985a73f7360e0af47016130206
Oasis14 <rycr0641@colorado.edu>
2017-12-03 20:31:35 -0700

    Added  door script to master

--- Assets/Scripts/doorScript.cs
+++ Assets/Scripts/doorScript.cs
@@ -2,24 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class doorScript : MonoBehaviour {
+public class doorScript : MonoBehaviour
+{
+    // Use this for initialization
+    public string doorState;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		openDoor ();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        changeDoorState(doorState);
 
-	void openDoor(){
-		var target = Quaternion.Euler (0, 90, 0);
-		transform.localRotation = Quaternion.Slerp (transform.localRotation, target, Time.deltaTime * 2);
-	}
+    }
 
-	void closeDoor(){
+    /* changeDoor(string state)
+	 * Called every update.
+	 * In order to change this the public variable doorState needs to be changed
+	 */
+    void changeDoorState(string state)
+    {
+        float rotation = 0;
+        int speed = 0;
+        string audioClip = "";
+        switch (state)
+        {
+            case "open":
+                rotation = 90;
+                speed = 5;
+                audioClip = "open";
+                break;
+            case "close":
+                rotation = 0;
+                speed = 5;
+                audioClip = "open";
+                break;
+            case "slam":
+                rotation = 0;
+                speed = 15;
+                audioClip = "slam";
+                break;
 
-	}
+            case "slowOpen":
+                rotation = 90;
+                speed = 1;
+                audioClip = "slowOpen";
+                break;
+        }
+        doorRotation(rotation, speed);
+    }
+
+    void doorRotation(float yRotation, int speed)
+    {
+        var target = Quaternion.Euler(0, yRotation, 0);
+        if (Quaternion.Angle(transform.localRotation, target) > 1)
+        {
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, target, speed);
+        }
+    }
+
+
 }
